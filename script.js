9e1129a5d4194372390d7be02d8687ea006dd1f2
lfer-29 <lfermin@caldwell.edu>
2026-02-08 12:04:25 -0500

    update?

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+function toggleDarkMode(){
+    if(localStorage.getItem('theme')=='dark')
+        localStorage.removeItem('theme');
+    else
+        localStorage.setItem('theme','dark');
+    setRootTheme();
+}
+function setRootTheme(){
+    const root=document.documentElement;
+    if(localStorage.getItem('theme')=='dark')
+        root.setAttribute('theme','dark');
+    else
+        root.removeAttribute('theme');
+}
+window.addEventListener('load',setRootTheme);
+
 const $ = document.querySelector.bind(document);
-$('#button1').addEventListener('click', function(event) {
-    console.log('hello world');
-});
+$('#button1').addEventListener('click', toggleDarkMode);
