e2ba4d1a9a8468b15d1bce7e00e0ebcd51594c7d
lfer-29 <lfermin@caldwell.edu>
2026-02-10 19:57:08 -0500

    update

--- lab-chef/script.js
+++ lab-chef/script.js
@@ -1,12 +1,32 @@
 const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent";
 
-function getRecipe() {
-    const apiKey = api-key-input.trim();
-    const ingredients = ingredients-input.trim();
-    if (apiKey = null) {
-        
+async function getRecipe() {
+    try {
+        const apiKeyInput = await response.getElementById('api-key-input'); 
+        const apiKey = apiKeyInput?.value.trim() || '';
+
+        const ingredientsInput = await response.getElementById('ingredients-input');
+        const ingredients = ingredientsInput?.value.trim() || '';
+    
+        if (!apiKey) {
+            console.error('API key is required.')
+            alert('Please enter an API key.')
+            return;
+        }
+        if (!ingredients) {
+            console.error('Ingredients are required.')
+            alert('Please enter at least one ingredient.')
+            return;
+        }
+    } catch (error) {
+    console.error('Error in getRecipe():', error);
+    alert('Error!');
+
+    console.log("ingredients", ingredients)
+    console.log("API", apiKey)
     }
-}
+} 
+
 
 const prompt = `I have these ingredients: ${ingredients}. Please provide a creative recipe name, a list of instructions, and estimated cooking time. Format the output in clean HTML (using <h2> and <li> tags). Return only the inner HTML content. Do not include markdown code blocks, and do not include <html>, <head>, or <body> tags. Start directly with an <h2> tag`;
 
@@ -19,6 +39,18 @@ fetch(API_URL, {
     body:JSON.stringify({contents:[{parts:[{text:prompt}]}]})
 })
 .then(r=>r.json())
-.then(getRecipe);
+.then(data=> {
+    if (
+        data &&
+        Array.isArray(data.candidates) &&
+        data.candidates[0]?.content?.parts &&
+        data.candidates[0].content.parts[0]?.text
+        ) {
+        document.getElementById('recipe-display').innerHTML =
+            data.candidates[0].content.parts[0].text;
+        } else {
+          document.getElementById('recipe-display').innerHTML =
+            'Unexpected data format.';
+        }
+});
 
-console.log("Hi!")
