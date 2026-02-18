2869aa083dc597a7a35b933144eff230940bab3b
lfer-29 <lfermin@caldwell.edu>
2026-02-18 14:17:27 -0500

    upd

--- lab4_express/src/express.js
+++ lab4_express/src/express.js
@@ -1,6 +1,6 @@
 const express = 
     require('express');
-const { getHits } = require('.hits.js');
+const { getHits } = require('./scr/hits.js');
 const app = express();
 app.get('/hits/:page', (req,res) => {
     res.contentType('text/plain').send (getHits());
